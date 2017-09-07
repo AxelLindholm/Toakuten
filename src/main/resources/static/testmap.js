23ad28e5ac83a43ecb73d9d2c340c01e59f35ddc
Marcus <splownrlive.se>
2017-09-07 13:44:12 +0200

    Fixed text

--- src/main/resources/static/testmap.js
+++ src/main/resources/static/testmap.js
@@ -162,6 +162,7 @@ function bindInfowindowWithMarker(listOfToilets, infowindow, index, response) {
 }
 function getTrueOrFalse(listOfToilets, index, mustPay, isHandicap, hasChangingTable) {
     google.maps.event.addListener(listOfToilets[index], 'click', function () {
+        $("#slide-sidebar").trigger('click');
         switch(mustPay){
             case true:
                 $(".mustpay").text("Pris: 5kr");
