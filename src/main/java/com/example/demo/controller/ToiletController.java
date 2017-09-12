ed8b557938aafa632534f5acb7ad88106905c2f4
Alice <alice.c@live.se>
2017-09-12 10:43:58 +0200

    Loading screen added.

--- src/main/java/com/example/demo/controller/ToiletController.java
+++ src/main/java/com/example/demo/controller/ToiletController.java
@@ -8,6 +8,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class ToiletController {
     //Woho
     @GetMapping("/")
+    public ModelAndView Index() {
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/map")
     public ModelAndView Googlemaps() {
         return new ModelAndView("Googlemaps");
     }
---
+++ src/main/resources/static/loading.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    var counter = 0;
+
+    // Set the percentage off
+    loading();
+});
+
+
+function loading(){
+    var num = 0;
+
+    for(i=0; i<=100; i++) {
+        setTimeout(function() {
+            $('.loader span').html(num+'%');
+
+            if(num == 100) {
+                window.location="/map";
+
+            }
+            num++;
+        },i*120);
+    };
+
+    $(window).load(function() {
+        $(".loader").show("");
+        $("/pictures/kut.gif").show("");
+
+    });
+
+
+}
